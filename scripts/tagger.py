c52fa2009ca0b3a7fc38dfbe3e062f935b1d259c
9492277468+-Aa <idiotcommerce@gmail.com>
2023-07-15 12:36:57 +0530

    their is a missing init.py file and file structer is edited

--- scripts/tagger.py
+++ scripts/tagger.py
@@ -3,7 +3,6 @@ from PIL import Image, ImageFile
 from modules import script_callbacks
 from tagger.api import on_app_started
 from tagger.ui import on_ui_tabs
-from tagger.settings import on_ui_settings
 
 
 # if you do not initialize the Image object
@@ -17,4 +16,3 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 script_callbacks.on_app_started(on_app_started)
 script_callbacks.on_ui_tabs(on_ui_tabs)
-script_callbacks.on_ui_settings(on_ui_settings)
