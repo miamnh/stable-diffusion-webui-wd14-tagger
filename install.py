f6bc2d32a3481281090d1b54c60646d23d3378b0
Roel Kluin <r.o.e.lkluin@gmail.com>
2023-07-15 21:40:55 +0200

    those are not needed, they come from the webui. installing a package named modules is even a serious problem

--- install.py
+++ install.py
@@ -8,6 +8,6 @@ NAME = "WD14-tagger"
 req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                         "requirements.txt")
 print(f"loading {NAME} reqs from {req_file}")
-run(f'"{sys.executable}" -m pip install -r "{req_file}"',
+run(f'"{sys.executable}" -m pip install -q -r "{req_file}"',
     f"Checking {NAME} requirements.",
     f"Couldn't install {NAME} requirements.")
