858cd889a5526c582d7fa211b7f50699ebbf7b56
Roel Kluin <r.o.e.lkluin@gmail.com>
2023-07-26 18:07:46 +0200

    The api model also needs to change. Allow a comma delimited list of interrogator to receive a combined results, fix some bugs.

--- tagger/api.py
+++ tagger/api.py
@@ -78,13 +78,12 @@ class Api:
         if req.image is None:
             raise HTTPException(404, 'Image not found')
 
-        if isinstance(req.model, list):
-            if any(i not in utils.interrogators.keys() for i in req.model):
-                raise HTTPException(404, 'Model {i} not found')
-
-        if req.model not in utils.interrogators.keys():
-            raise HTTPException(404, 'Model not found')
-            req.model = [req.model]
+        req_models = []
+        for i in map(str.strip, req.model.split(',')):
+            if i in utils.interrogators.keys():
+                req_models.append(i)
+            else:
+                raise HTTPException(404, f"Model '{i}' not found")
 
         image = decode_base64_to_image(req.image)
         QData.tags.clear()
@@ -100,7 +99,7 @@ class Api:
         if req.count_threshold:
             QData.count_threshold = req.count_threshold
 
-        for model in req.model:
+        for model in req_models:
             interrogator = utils.interrogators[model]
             with self.queue_lock:
                 data = ('', '', '') + interrogator.interrogate(image)
