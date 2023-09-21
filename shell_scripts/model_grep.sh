f5831a117f9eadf085df8b44ab90a32c0b462501
Roel Kluin <r.o.e.lkluin@gmail.com>
2023-09-21 23:41:05 +0200

    no duplicates

--- shell_scripts/model_grep.sh
+++ shell_scripts/model_grep.sh
@@ -39,13 +39,13 @@ fi
 if [ -n "$1" ]; then
     echo "reading from $1 with $ncpu cpus" 1>&2
     ls -1 ${path}/*.safetensors | parallel -n 1 -j $ncpu "python ${utilpath}/safetensors_util.py metadata {} -pm 2>/dev/null |
-    sed -n '1b;p' | jq '.__metadata__.ss_tag_frequency' 2>/dev/null | grep -o -E '\"[^\"]*${1}[^\"]*\": [0-9]+'| sed 's~^~'{}':~p'"
+    sed -n '1b;p' | jq '.__metadata__.ss_tag_frequency' 2>/dev/null | grep -o -E '\"[^\"]*${1}[^\"]*\": [0-9]+'| sed 's~^~'{}':~'"
 else
     echo "reading from stdin" 1>&2
     tmp=$(mktemp)
     sed 's/^/"[^\"]*/;s/$/[^\"]*": [0-9]+/' < /dev/stdin > $tmp
     ls -1 ${path}/*.safetensors | parallel -n 1 -j $ncpu "python ${utilpath}/safetensors_util.py metadata {} -pm 2>/dev/null |
-    sed -n '1b;p' | jq '.__metadata__.ss_tag_frequency' 2>/dev/null | grep -oE -f $tmp | sed 's~^~'{}':~p'"
+    sed -n '1b;p' | jq '.__metadata__.ss_tag_frequency' 2>/dev/null | grep -oE -f $tmp | sed 's~^~'{}':~'"
     echo rm $tmp
 fi
 
